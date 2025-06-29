6eef5fe3e558d0014b6769ba1e047e6308040b39
SuhaniMadhwal <161447945+SuhaniMadhwal@users.noreply.github.com>
2025-06-29 15:26:57 +0000

    operations

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -77,7 +77,52 @@
 // "Suhani"=>true
 
 
-let someNumber = 33
-let StringNumber = String(someNumber)
-console.log(StringNumber); //33
-console.log(typeof StringNumber);  //String
+// let someNumber = 33
+// let StringNumber = String(someNumber)
+// console.log(StringNumber); //33
+// console.log(typeof StringNumber);  //String
+
+
+
+
+// ***************OPERATIONS*****************
+
+// let value=3
+// let negvalue =-value
+// console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2%2);
+// console.log(2**2); //to the power
+// console.log(2/2);
+
+
+
+let st1= "Suhani"
+let st2 ="Madhwal"
+
+let st3= st1 + st2
+// console.log(st3);
+
+// console.log("1" + 2);  //12
+// console.log(1 + "2");  //12
+// console.log("1" + 2 + 2);  //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log((3+4)*5%3);
+
+
+// console.log(true); //true
+// console.log(+true);  //1
+// console.log(true+); //error
+// console.log(+"");  //1 0
+
+
+//  let num1,num2,num3
+// num1=num2=num3= 2+2
+
+letgameCounter=100
+gameCounter++;
+comsole.log(gameCounter);
