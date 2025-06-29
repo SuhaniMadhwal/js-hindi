07469da38829ba9b45d0619fd6893d8996b51865
SuhaniMadhwal <161447945+SuhaniMadhwal@users.noreply.github.com>
2025-06-29 16:29:14 +0000

    comparison

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2!=1);
+
+
+console.log("2">1); //true
+console.log("02">1); //true
+
+
+console.log(null>0); //false
+console.log(null==0); //false
+console.log(null>=0);  //true
+
+
+console.log(undefined ==0);
+console.log(undefined >=0);
+console.log(undefined >0); //always false
+
+
+// ===  strict check, checks data type also
+ console.log("2" ==2);
+ console.log("2"===2); //data type not same
