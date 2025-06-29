72afccd1a93e1f62396b2aa36966be6bf2a0111c
SuhaniMadhwal <161447945+SuhaniMadhwal@users.noreply.github.com>
2025-06-29 14:11:01 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) we are using nodejs, nor browser
+
+//console.log( 3+3)  console.log("Suhani") // error
+
+// code readability should be high
+
+let name = "Suhani"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to the power 53
+//bigint
+//string => ""
+//boolean => true or false
+//null => standalone value
+// undefined =>
+// symbol=> fine uniqueness
+
+//object
+
+console.log(typeof "suhani");
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
