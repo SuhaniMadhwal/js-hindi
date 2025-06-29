cdd99a872b4a1c3aadbb90da7952a2f6758f6899
SuhaniMadhwal <161447945+SuhaniMadhwal@users.noreply.github.com>
2025-06-29 15:11:11 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,83 @@
+// let score = "33"
+//  console.log(typeof score);
+//  console.log(typeof (score));
+
+//  let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+
+
+
+// let score = "33abc"
+//  console.log(typeof score);
+//  console.log(typeof (score));
+
+//  let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+
+
+
+//  let score = null
+//  console.log(typeof score);
+//  console.log(typeof (score));
+
+//  let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+
+
+
+//  let score = undefined
+//  console.log(typeof score);
+//  console.log(typeof (score));
+
+//  let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+
+
+
+//  let score = true
+//  console.log(typeof score);
+//  console.log(typeof (score));
+
+//  let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+
+
+
+//   let score = "Suhani"
+//  console.log(typeof score);
+//  console.log(typeof (score));
+
+//  let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+
+ 
+ // "33" => 33
+ //"33abc"=> Nan
+ // true=>1; false=>=0
+
+
+//  let isLoggedIn = 1
+//  let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+
+//  let isLoggedIn = "Suhani"
+//  let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+
+//1=> true; 0=>false
+// ""=> false
+// "Suhani"=>true
+
+
+let someNumber = 33
+let StringNumber = String(someNumber)
+console.log(StringNumber); //33
+console.log(typeof StringNumber);  //String
