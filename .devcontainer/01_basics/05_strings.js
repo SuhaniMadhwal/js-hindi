80d97496287f26201543a78b4276d116bbcdce29
SuhaniMadhwal <161447945+SuhaniMadhwal@users.noreply.github.com>
2025-06-30 15:28:54 +0000

    strings

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,34 @@
+const name = "Suhani"
+const repoc = 50
+// console.log(name + repoc);
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoc}`);
+
+const gamename = new String('Suhani-M')
+
+// console.log(gamename[0]);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.length);
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf('S'));
+
+
+const newstr = gamename.substring(0,4)
+// console.log(newstr);
+
+
+ const anotstr = gamename.slice(-10,4)
+//  console.log(anotstr);
+
+
+const newstring = "      Sm     "
+// console.log(newstring);
+// console.log(newstring.trim());
+
+
+const url = "https://Suhanim.com/     %20"
+// console.log(url.replace('%20', 'Kandholi'));
+
+// console.log(url.includes('Kandholi'));
+
+// console.log(gamename.split(' '));
